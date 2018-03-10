a6ea19e2641cb0f96b48a468a47cc9648bdf80eb
luoziyong <luoziyong@msn.com>
2018-03-10 21:18:13 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+from rest import KK_REST
+
+api_key = ''
+api_secret = open('yourprivate.key').read()
+password = ''  # RSA private key password
+
+kk_rest = KK_REST(api_key, api_secret, password)
+
+symbol = 'KK_ETH'
+order_type = 'LIMIT'
+order_op = 'BUY'
+price = 0.001
+amount = 1000
+
+result = kk_rest.trade(symbol, order_type, order_op, price, amount)
+print result
+
+result = kk_rest.balance()
+print result
